ff976d2660ed47b8809c2e25a5a56c930edf7a17
Amir Hosseinzadeh <amir.hosseinzadeh@c2ro.com>
2018-09-28 12:23:54 -0400

    add simple integration tests

---
+++ src/test/java/com/upgrade/volcano/ReservationControllerTest.java
@@ -0,0 +1,56 @@
+package com.upgrade.volcano;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootApplication
+class ApplicationTest {
+}
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ReservationControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void givenWac_whenServletContext_thenItProvidesReservationController() {
+        ServletContext servletContext = wac.getServletContext();
+
+        Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(wac.getBean("reservationController"));
+    }
+
+    @Test
+    public void givenHomePageURI_whenMockMVC_thenReturnsOk() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    }
+
+
+}
